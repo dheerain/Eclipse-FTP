1e8bf3c29edf82a1d6d39d2345bf563600442b86
Dheerain Gandhi <dheerain2902@gmail.com>
2017-01-12 15:59:11 +0530

    New information is Added

--- Eclipse_FTP.bat
+++ Eclipse_FTP.bat
@@ -1,23 +1,24 @@
 @echo Off
 REM ******************************************************************************************************
-REM *                       ECLIPSE COMPILER AND PUSH                                                    *
+REM *                       ECLIPSE PUSH                                                                 *
 REM ******************************************************************************************************        
 REM * FOR THE FIRST RUN SET THE BELOW 4 VARIABLES WITH YOUR UNIX ACCOUNT DETAILS                         *
 REM * USERNAME=UNIX-USERNAME                                                                             *
 REM * PASSWORD=UNIX-PASSWORD                                                                             *
 REM * HOST=MACHINE-NAME                                                                                  *
-REM * UNIXPATH="Path of the Unix machine of the current version you are working on till 'ENS' Directory" *
+REM * ROOTPATH="Path on the machine with root of the project "                                           *
 REM ******************************************************************************************************
-REM *  JAVA 1.7 IS REQUIRED
+REM *  JAVA 1.7 IS REQUIRED--Not Needed
+REM *  Make Sure No extra charactors are there in the variable
 REM ******************************************************************************************************
-set username=dgandhi
-set password=Nov_2016
-set host=hpx619
-set unixpath=/mtpdevuser1/mtp/cc/dgandhi/dgandhi_hpx619_2000-Release/ens%1
+set username=???
+set password=???
+set host=??
+set rootpath=???
+REM ******************************************************************************************************
 
+set unixpath=%rootpath%1
 echo "Placing the Files to the respective directory"
-REM echo "Unix Path is <%1>"
-REM echo "Windows Path is <%2>"
 SET mypath=%~dp0
 set "unixpath=%unixpath:\=/%"
 REM echo "Unix File Path is <%unixpath%>"           
@@ -28,8 +29,9 @@ echo put %2 %unixpath%>>"%mypath%\a.ftp"
 echo disconnect>>"%mypath%\a.ftp" 
 echo quit>>"%mypath%\a.ftp"    
 C:\Windows\System32\ftp.exe -s:"%mypath%\a.ftp"   
+C:\Windows\System32\ftp.exe -s:"%mypath%\a.ftp"   
 
-java -jar "%mypath%\TriggerBuild.jar" %username% %password% %host% %unixpath%
+REM java -jar "%mypath%\TriggerBuild.jar" %username% %password% %host% %unixpath%
 del "%mypath%\a.ftp"
 
 
